d8ea0742b442a5fd70b00c9ef52cfda937d1852a
josé <jlc.beker@gmail.com>
2023-09-10 10:26:30 -0300

    test app (needs linking)

---
+++ tests/shellkit-test-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ShellKitTerminal from "../../src/index.js"
+function App() {
+  return (
+    <div className="App">
+      <h1>shellkit Testing App</h1>
+      <ShellKitTerminal />
+    </div>
+  );
+}
+
+export default App;
